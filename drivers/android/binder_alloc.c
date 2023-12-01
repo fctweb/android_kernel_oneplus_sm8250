414624848f9e7b8f1ac24caa223d1852f7813ddd
Carlos Llamas <cmllamas@google.com>
2023-12-01 17:21:33 +0000

    binder: fix async space check for 0-sized buffers
    commit 3091c21d3e9322428691ce0b7a0cfa9c0b239eeb upstream.
    
    Move the padding of 0-sized buffers to an earlier stage to account for
    this round up during the alloc->free_async_space check.
    
    Fixes: 74310e06be4d ("android: binder: Move buffer out of area shared with user space")
    Reviewed-by: Alice Ryhl <aliceryhl@google.com>
    Signed-off-by: Carlos Llamas <cmllamas@google.com>
    Link: https://lore.kernel.org/r/20231201172212.1813387-5-cmllamas@google.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/android/binder_alloc.c
+++ drivers/android/binder_alloc.c
@@ -430,9 +430,13 @@ static struct binder_buffer *binder_alloc_new_buf_locked(
 				alloc->pid, extra_buffers_size);
 		return ERR_PTR(-EINVAL);
 	}
+
+	/* Pad 0-size buffers so they get assigned unique addresses */
+	size = max(size, sizeof(void *));
+
 #ifdef OPLUS_FEATURE_HANS_FREEZE
 	if (is_async
-		&& (alloc->free_async_space < 3 * (size + sizeof(struct binder_buffer))
+		&& (alloc->free_async_space < 3 * size
 		|| (alloc->free_async_space < ((alloc->buffer_size / 2) * 9 / 10)))) {
 		rcu_read_lock();
 		p = find_task_by_vpid(alloc->pid);
@@ -442,16 +446,13 @@ static struct binder_buffer *binder_alloc_new_buf_locked(
 		}
 	}
 #endif /*OPLUS_FEATURE_HANS_FREEZE*/
-	if (is_async &&
-	    alloc->free_async_space < size + sizeof(struct binder_buffer)) {
+
+	if (is_async && alloc->free_async_space < size) {
 		binder_alloc_debug(BINDER_DEBUG_BUFFER_ALLOC,
 			     "%d: binder_alloc_buf size %zd failed, no async space left\n",
 			      alloc->pid, size);
 		return ERR_PTR(-ENOSPC);
 	}
-
-	/* Pad 0-size buffers so they get assigned unique addresses */
-	size = max(size, sizeof(void *));
 
 	while (n) {
 		buffer = rb_entry(n, struct binder_buffer, rb_node);
